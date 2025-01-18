280a86bc222b00c659197b214fa8609fdae895b5
Programeowr <programeowr@gmail.com>
2025-01-18 15:39:44 +0530

    Update MiniGames-Finished

--- EcoSplash/Assets/Scripts/FishingScripts/NetScript.cs
+++ EcoSplash/Assets/Scripts/FishingScripts/NetScript.cs
@@ -5,7 +5,14 @@ public class NetScript : MonoBehaviour
     private float minX = -12f;
     private float maxX = 11f;
     private float minY = -7f;
-    private float maxY = -3f;
+    private float maxY = 3f;
+
+    private float netX = 1f;
+    private float netY = 1.5f;
+
+    private bool isCaught = false;
+
+    GameObject caughtObject;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,23 +22,52 @@ public class NetScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 mousePos = Input.mousePosition;
+        followMouse();
 
+        if (isCaught)
+        {
+            caughtObject.GetComponent<Transform>().position = this.transform.position + new Vector3(netX, netY, 1);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+                Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
+
+                if (hit.collider != null && hit.collider.name == "Dustbin")
+                {
+                    isCaught = false;
+                    Destroy(caughtObject);
+                }
+          
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (isCaught)
+        {
+            return;
+        }
+
+        if(collision.gameObject.tag == "Garbage")
+        {
+            caughtObject = collision.gameObject;
+            Debug.Log("Collision detected");
+            isCaught = true;    
+        }
+    }
+
+    private void followMouse()
+    {
+        
+        Vector3 mousePos = Input.mousePosition;
         mousePos = Camera.main.ScreenToWorldPoint(mousePos);
 
         mousePos.z = transform.position.z;
         mousePos.x = Mathf.Clamp(mousePos.x, minX, maxX);
         mousePos.y = Mathf.Clamp(mousePos.y, minY, maxY);
         transform.position = mousePos;
-
-        
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag == "Garbage")
-        {
-            collision.gameObject.SetActive(false);
-        }
     }
 }
