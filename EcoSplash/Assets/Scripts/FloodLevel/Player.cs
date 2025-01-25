0dc2741e3992a71564326fb1660641e97e435613
Karri Sri Saya Sandeep <srisayasandeepkarri@gmail.com>
2025-01-25 16:46:10 +0530

    Revert "Reapply "Merge pull request #53 from VasantatiChitraksh/sathvik""
    This reverts commit 7b8be011eda8752cb6c98774bef16ea5861cb6b6.

---
+++ EcoSplash/Assets/Scripts/FloodLevel/Player.cs
@@ -0,0 +1,94 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private GameInput gameInput;
+    private bool isWalking;
+
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float jumpForce = 5f; // Jumping force
+    private bool isGrounded; // To check if the player is on the ground
+    private Rigidbody playerRigidbody; // Rigidbody for physics-based movement
+
+    void Start()
+    {
+        // Get the Rigidbody component
+        playerRigidbody = GetComponent<Rigidbody>();
+        if (playerRigidbody == null)
+        {
+            Debug.LogError("Rigidbody not found on the player object.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+        HandleJump();
+    }
+
+    private void HandleMovement()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        float playerHeight = 2f;
+        float playerRadius = .7f;
+        float moveDistance = moveSpeed * Time.deltaTime;
+
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        if (!canMove)
+        {
+            // Attempt movement on X direction
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0);
+            canMove = moveDir.x != 0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            if (canMove)
+            {
+                moveDir = moveDirX;
+            }
+            else
+            {
+                // Attempt movement on Z direction
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z);
+                canMove = moveDir.z != 0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                if (canMove)
+                {
+                    moveDir = moveDirZ;
+                }
+            }
+        }
+
+        if (canMove)
+        {
+            transform.position += moveDir * moveDistance;
+        }
+
+        isWalking = moveDir != Vector3.zero;
+
+        float rotateSpeed = 10f;
+        if (isWalking)
+        {
+            transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+        }
+    }
+
+    private void HandleJump()
+    {
+        // Check if the jump input is pressed and the player is grounded
+        if (gameInput.GetJumpInput() && isGrounded)
+        {
+            playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false; // Prevent multiple jumps
+        }
+    }
+
+    // Unity's built-in methods for collision detection
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
---
+++ EcoSplash/Assets/_Assets/RainMaker/Prefab/RainScript.cs
@@ -0,0 +1,74 @@
+﻿//
+// Rain Maker (c) 2015 Digital Ruby, LLC
+// http://www.digitalruby.com
+//
+
+using UnityEngine;
+using System.Collections;
+
+namespace DigitalRuby.RainMaker
+{
+    public class RainScript : BaseRainScript
+    {
+        [Tooltip("The height above the camera that the rain will start falling from")]
+        public float RainHeight = 25.0f;
+
+        [Tooltip("How far the rain particle system is ahead of the player")]
+        public float RainForwardOffset = -7.0f;
+
+        [Tooltip("The top y value of the mist particles")]
+        public float RainMistHeight = 3.0f;
+
+        private void UpdateRain()
+        {
+            if (Camera == null)
+            {
+                Camera = Camera.main;
+            }
+
+            if (Camera == null)
+            {
+                Debug.LogError("No camera available for rain script");
+                enabled = false;
+                return;
+            }
+
+            if (RainFallParticleSystem != null)
+            {
+                var s = RainFallParticleSystem.shape;
+                s.shapeType = ParticleSystemShapeType.ConeVolume;
+
+                if (FollowCamera)
+                {
+                    RainFallParticleSystem.transform.position = Camera.transform.position;
+                    RainFallParticleSystem.transform.Translate(0.0f, RainHeight, RainForwardOffset);
+                    RainFallParticleSystem.transform.rotation = Quaternion.Euler(0.0f, Camera.transform.rotation.eulerAngles.y, 0.0f);
+                }
+
+                if (RainMistParticleSystem != null)
+                {
+                    var s2 = RainMistParticleSystem.shape;
+                    s2.shapeType = FollowCamera ? ParticleSystemShapeType.Hemisphere : ParticleSystemShapeType.Box;
+
+                    Vector3 pos = FollowCamera 
+                        ? Camera.transform.position 
+                        : RainFallParticleSystem.transform.position;
+
+                    pos.y += RainMistHeight;
+                    RainMistParticleSystem.transform.position = pos;
+                }
+            }
+        }
+        protected override void Start()
+        {
+            base.Start();
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+
+            UpdateRain();
+        }
+    }
+}
