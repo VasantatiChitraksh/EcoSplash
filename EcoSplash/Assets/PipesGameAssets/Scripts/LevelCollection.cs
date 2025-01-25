cd23c3bc8aa81dbef550d56696fd2f37ecaafa23
Karri Sri Saya Sandeep <srisayasandeepkarri@gmail.com>
2025-01-25 17:18:08 +0530

    Deleted Buggy Game

--- EcoSplash/Assets/PipesGameAssets/Scripts/LevelCollection.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "AllLevels", menuName = "Level/Level Collection")]
-public class LevelCollection : ScriptableObject
-{
-    public List<LevelData> levels = new List<LevelData>();
-}
